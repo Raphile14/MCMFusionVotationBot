1f0c65dabcb704e11591998c80d822faadec6244
Raphile14 <rllddalangin@gmail.com>
2020-09-23 09:06:44 +0800

    indev 0.1.0 (09/23/2020)

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MCMFusionVotationBot
+
+indev 0.1.0 (09/23/2020)
+- Intializing Project
---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+// Allows the process of data
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// ROUTES
+app.get('/', function(req, res){
+    res.send("Hi I am a chatbot");
+});
+
+// Facebook
+app.get('/webhook/', function(req, res){
+    if (req.query['hub.verify_token'] === "april142000") {
+        res.send(req.query['hub.challenge']);
+    }
+    res.send("Wrong token");
+});
+
+app.listen(app.get('port'), function(){
+    console.log("running: port");
+})
