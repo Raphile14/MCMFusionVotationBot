b54d894f2365c55ec228adc7f0b7a08f039cdc6a
Raphile14 <rllddalangin@gmail.com>
2020-09-26 07:46:22 +0800

    indev 0.3.3-a (09/25/2020)

--- client/js/script.js
+++ client/js/script.js
@@ -4,6 +4,9 @@ socket.on("usersOnline", function(data){
     document.getElementById("usersConnected").innerText =  data.number;
 });
 
-socket.on("test", function(){
-    console.log("test received");
+socket.on("scoreUpdate", function(data){
+    for (var key in data.count) {
+        document.getElementById(key).innerText =  data.count[key];
+        console.log(key);
+    }
 })
