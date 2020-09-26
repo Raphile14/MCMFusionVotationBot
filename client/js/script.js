2df5f3c1045d905612a94d1af618b3bc4c96f092
Raphile14 <rllddalangin@gmail.com>
2020-09-26 08:12:27 +0800

    indev 0.3.3-f (09/26/2020)

--- client/js/script.js
+++ client/js/script.js
@@ -6,6 +6,7 @@ socket.on("usersOnline", function(data){
 
 socket.on("test", function(data){
     console.log("Received a score update!")
+    console.log(data);
     console.log(data[0]);
     for (var key in data[0]) {
         document.getElementById(key).innerText =  data[0][key];
